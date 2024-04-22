3f869c400d7465b9fd34634125f5b07c0b57a873
Moo!! <zzzzz@znoz.com>
2024-04-22 14:54:49 +0900

    Update average.php

--- Q3/average.php
+++ Q3/average.php
@@ -1,17 +1,27 @@
 <?php
-$servername = "your_server_name";
-$username = "your_username";
-$password = "your_password";
-$dbname = "your_db_name";
+function GetAverage() {
+    $servername = "your_server_name";
+    $username = "your_username";
+    $password = "your_password";
+    $dbname = "your_db_name";
+    
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
+    
+    $sql = "SELECT AVG(survivalRate) as Average FROM Picks";
+    $result = $conn->query($sql);
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
+    $average = 0;
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $average = $row["Average"];
+    }
+    $conn->close();
 
-$sql = "SELECT AVG(survivalRate) as Average FROM Picks";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    $row = $result->fetch_assoc();
-    echo "Average Survival Rate: " . $row["Average"]. "<br>";
+    return $average;
 }
-$conn->close();
+
+// 샘플 실행
+$average = GetAverage();
+
+printf("평균 성공률: %d", $average);
